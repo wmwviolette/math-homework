7208cbb883ceec6f98898524e7a566754f1036f1
wmwviolette <wmwviolette@gmx.com>
2025-03-06 09:56:59 +0000

    Add 2.js

---
+++ src/2.js
@@ -0,0 +1,27 @@
+ const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+// Add two numbers
+add(2, 3);
+
+// Subtract one number from another
+subtract(4, 2);
+
+// Multiply two numbers
+multiply(5, 2);
+
+// Divide one number by another
+divide(10, 2);
